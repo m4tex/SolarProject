1b797c234819f5795e2c75d28d262a7c38419c51
m4tex <91064036+m4tex@users.noreply.github.com>
2022-05-29 00:54:52 +0200

    Init commit

---
+++ main.cpp
@@ -0,0 +1,91 @@
+#ifndef UNICODE
+#define UNICODE
+#endif
+
+#include <windows.h>
+
+LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
+
+int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PWSTR pCmdLine, int nCmdShow)
+{
+    // Register the window class.
+    const wchar_t CLASS_NAME[]  = L"Sample Window Class";
+
+    WNDCLASS wc = { };
+
+    wc.lpfnWndProc   = WindowProc;
+    wc.hInstance     = hInstance;
+    wc.lpszClassName = CLASS_NAME;
+
+    RegisterClass(&wc);
+
+    // Create the window.
+
+    HWND hwnd = CreateWindowEx(
+            0,                              // Optional window styles.
+            CLASS_NAME,                     // Window class
+            L"Learn to Program Windows",    // Window text
+            WS_OVERLAPPEDWINDOW,            // Window style
+
+            // Size and position
+            CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT,
+
+            NULL,       // Parent window
+            NULL,       // Menu
+            hInstance,  // Instance handle
+            NULL        // Additional application data
+    );
+
+    if (hwnd == NULL)
+    {
+        return 0;
+    }
+
+    ShowWindow(hwnd, nCmdShow);
+
+    // Run the message loop.
+
+    MSG msg = { };
+    while (GetMessage(&msg, NULL, 0, 0) > 0)
+    {
+        TranslateMessage(&msg);
+        DispatchMessage(&msg);
+    }
+
+    return 0;
+}
+
+void DrawFrame(HDC hdc) {
+    MoveToEx(hdc, 50, 50, NULL);
+    LineTo(hdc, 25, 25);
+}
+
+LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
+{
+    switch (uMsg)
+    {
+        case WM_DESTROY:
+            PostQuitMessage(0);
+            return 0;
+
+        case WM_PAINT:
+        {
+            PAINTSTRUCT ps;
+            HDC hdc = BeginPaint(hwnd, &ps);
+            auto hPen = CreatePen(PS_DASHDOTDOT, 2, RGB(255, 0, 0));
+            auto hBrush = CreateSolidBrush(RGB(0, 255, 0));
+            SelectObject(hdc, hPen);
+            Rectangle(hdc, 16, 36, 72, 70);
+            Rectangle(hdc, 0, 0, 50, 50);
+            MoveToEx(hdc, 50, 50, NULL);
+            LineTo(hdc, 25, 25);
+
+
+            EndPaint(hwnd, &ps);
+        }
+            return 0;
+
+    }
+
+    return DefWindowProc(hwnd, uMsg, wParam, lParam);
+}
