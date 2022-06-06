b3fc3eaa2de7e6903d6f457c6d047197e1ee77c5
m4tex <91064036+m4tex@users.noreply.github.com>
2022-06-06 03:19:57 +0200

    Rendering a cube :3 Started working on normals

--- src/main.cpp
+++ src/main.cpp
@@ -11,23 +11,35 @@ struct Vector3 {
     float x, y, z;
 
     Vector3 operator*(float amount) {
-        return { x*amount, y*amount, z*amount };
+        return {x * amount, y * amount, z * amount};
     }
 
     Vector3 operator*(Vector3 vec) {
-        return { x*vec.x, y*vec.y, z*vec.z };
+        return {x * vec.x, y * vec.y, z * vec.z};
     }
 
     void operator*=(Vector3 vec) {
-        x*=vec.x; y*=vec.y; z*=vec.z;
+        x *= vec.x;
+        y *= vec.y;
+        z *= vec.z;
     }
 
     void operator+=(float amount) {
-        x+=amount; y+=amount; z+=amount;
+        x += amount;
+        y += amount;
+        z += amount;
     }
 
     void operator+=(Vector3 vec) {
-        x+=vec.x; y+=vec.y; z+=vec.z;
+        x += vec.x;
+        y += vec.y;
+        z += vec.z;
+    }
+
+    void operator/=(float amount) {
+        x /= amount;
+        y /= amount;
+        z /= amount;
     }
 };
 
@@ -40,7 +52,7 @@ struct Mesh {
 };
 
 struct Matrix4x4 {
-    float m[4][4] = { 0 };
+    float m[4][4] = {0};
 };
 
 void StartWindow(HINSTANCE hinstance);
@@ -79,7 +91,7 @@ void StartWindow(HINSTANCE hinstance) {
             WS_OVERLAPPEDWINDOW & ~WS_THICKFRAME,
 
             // Size and position
-            (screenWidth/2)-263, (screenHeight/2)-263, wnd_w, wnd_h+31,
+            (screenWidth / 2) - 263, (screenHeight / 2) - 263, wnd_w, wnd_h + 31,
 
             nullptr,       // Parent window
             nullptr,       // Menu
@@ -111,7 +123,7 @@ void DrawFrame(HDC deviceCtx);
 
 void EngineTick();
 
-void DrawTriangle3DSpace(Triangle triangle, Vector3 pos, HDC deviceCtx);
+void DrawMesh(Mesh mesh, HDC deviceCtx);
 
 LRESULT CALLBACK WindowProc(HWND windowHandler, UINT uMsg, WPARAM wParam, LPARAM lParam) {
     switch (uMsg) {
@@ -134,18 +146,48 @@ HBRUSH blackBrush = CreateSolidBrush(RGB(0, 0, 0));
 HBRUSH whiteBrush = CreateSolidBrush(RGB(255, 255, 255));
 HPEN whitePen = CreatePen(0, 1, RGB(255, 255, 255));
 
-Vector3 position = {100, 100, 0};
-Vector3 scale = { 10, 10, 10 };
+Vector3 camPos;
+Vector3 position = {-.5, -.5, 1};
+Vector3 scale = {1, 1, 1};
 Vector3 angularVelocity = {1, 0, 0};
 Vector3 velocity = {1, 1, 0};
 
-Triangle triangle = {1, 1, 0, 3, 1, 0, 2, 2, 0 };
+//#region cube
+const Mesh dummy_cube = {
+        .tris {
+                // SOUTH
+                {0, 0, 0, 0, 1, 0, 1, 1, 0},
+                {0, 0, 0, 1, 1, 0, 1, 0, 0},
+
+                // EAST
+                {1, 0, 0, 1, 1, 0, 1, 1, 1},
+                {1, 0, 0, 1, 1, 1, 1, 0, 1},
+
+                // NORTH
+                {1, 0, 1, 1, 1, 1, 0, 1, 1},
+                {1, 0, 1, 0, 1, 1, 0, 0, 1},
+
+                // WEST
+                {0, 0, 1, 0, 1, 1, 0, 1, 0},
+                {0, 0, 1, 0, 1, 0, 0, 0, 0},
+
+                // TOP
+                {0, 1, 0, 0, 1, 1, 1, 1, 1},
+                {0, 1, 0, 1, 1, 1, 1, 1, 0},
+
+                // BOTTOM
+
+                {1, 0, 1, 0, 0, 0, 1, 0, 0},
+                {1, 0, 1, 0, 0, 1, 0, 0, 0},
+        }
+};
+//#endregion
 
 void DrawFrame(HDC deviceCtx) {
     FillRect(deviceCtx, &clientArea, blackBrush);
     SelectObject(deviceCtx, whitePen);
 
-    DrawTriangle3DSpace(triangle, position, deviceCtx);
+    DrawMesh(dummy_cube, deviceCtx);
 }
 
 void MultiplyMatrixVector(Vector3 &i, Vector3 &o, Matrix4x4 &m);
@@ -161,23 +203,64 @@ Matrix4x4 projectionMatrix = {
         aspectRatio * fovRad, 0, 0, 0,
         0, fovRad, 0, 0,
         0, 0, zFar / (zFar - zNear), 1.0f,
-        0, 0, (-zFar * zNear) / (zFar - zNear), 0 };
+        0, 0, (-zFar * zNear) / (zFar - zNear), 0};
 
 void DrawTriangle3DSpace(Triangle triangle, Vector3 pos, HDC deviceCtx) {
     Triangle projected;
 
+    //Translation and scaling
     for (int i = 0; i < 3; ++i) {
         triangle.vertices[i] *= scale;
         triangle.vertices[i] += pos;
-        MultiplyMatrixVector(triangle.vertices[i], projected.vertices[i], projectionMatrix);
     }
 
+    //calculate normal and normalize it
+    Vector3 normal, startLine, endLine;
+    startLine.x = triangle.vertices[1].x - triangle.vertices[0].x;
+    startLine.y = triangle.vertices[1].y - triangle.vertices[0].y;
+    startLine.z = triangle.vertices[1].z - triangle.vertices[0].z;
+
+    endLine.x = triangle.vertices[2].x - triangle.vertices[0].x;
+    endLine.y = triangle.vertices[2].y - triangle.vertices[0].y;
+    endLine.z = triangle.vertices[2].z - triangle.vertices[0].z;
+
+    normal.x = startLine.y * endLine.z - startLine.z * endLine.y;
+    normal.x = startLine.z * endLine.x - startLine.x * endLine.z;
+    normal.x = startLine.x * endLine.y - startLine.y * endLine.x;
+
+    float length = sqrtf(normal.x * normal.x + normal.y * normal.y + normal.z * normal.z);
+
+    normal /= length;
+
+    //Project to 3D if visible (normal relative to camera is negative)
+    if (normal.x * (triangle.vertices[0].x - camPos.x) +
+        normal.y * (triangle.vertices[0].y - camPos.y) +
+        normal.z * (triangle.vertices[0].z - camPos.z) < 0) {
+        for (int i = 0; i < 3; ++i) {
+            MultiplyMatrixVector(triangle.vertices[i], projected.vertices[i], projectionMatrix);
+        }
+    }
+
+    //scale into view
+    for (int i = 0; i < 3; ++i) {
+        projected.vertices[i] += 1;
+        projected.vertices[i].x *= 0.5f * wnd_w;
+        projected.vertices[i].y *= 0.5f * wnd_h;
+    }
+
+    //Draw the result
     MoveToEx(deviceCtx, projected.vertices[0].x, clientArea.bottom - projected.vertices[0].y, nullptr);
     LineTo(deviceCtx, projected.vertices[1].x, clientArea.bottom - projected.vertices[1].y);
     MoveToEx(deviceCtx, projected.vertices[1].x, clientArea.bottom - projected.vertices[1].y, nullptr);
     LineTo(deviceCtx, projected.vertices[2].x, clientArea.bottom - projected.vertices[2].y);
     MoveToEx(deviceCtx, projected.vertices[2].x, clientArea.bottom - projected.vertices[2].y, nullptr);
     LineTo(deviceCtx, projected.vertices[0].x, clientArea.bottom - projected.vertices[0].y);
+}
+
+void DrawMesh(Mesh mesh, HDC deviceCtx) {
+    for (auto triangle: mesh.tris) {
+        DrawTriangle3DSpace(triangle, position, deviceCtx);
+    }
 }
 
 void EngineTick() {
@@ -191,6 +274,8 @@ void MultiplyMatrixVector(Vector3 &i, Vector3 &o, Matrix4x4 &m) {
     float w = i.x * m.m[0][3] + i.y * m.m[1][3] + i.z * m.m[2][3] + m.m[3][3];
 
     if (w != 0) {
-        o.x /= w; o.y /= w; o.z /= w;
+        o.x /= w;
+        o.y /= w;
+        o.z /= w;
     }
 }
